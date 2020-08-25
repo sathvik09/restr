aa86404e107bb7101b94a8d09c99b540aafeb670
sathvik09 <47394244+sathvik09@users.noreply.github.com>
2020-08-25 19:15:21 +0530

    ji

--- npmresf/landing/index.php
+++ npmresf/landing/index.php
@@ -8,8 +8,10 @@
       <link rel="stylesheet" href="./main.css?version=23">
       <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
-<body>
-      <div class="hero-back">
+<body onload="workItt">
+<div id="loader"><div id="loader2"></div></div>
+<div class= "animateBottom" id="whenDone" style="width:100vw;">
+<div class="hero-back">
             <img style="width: 20%;height:10vh;" src="../pics/gourmet.png" alt="">
       </div>
       <div class="content">
@@ -19,6 +21,24 @@
                <input type="submit" name = "s3" value="free">
             </form>
        </div>
+</div>
+       <script>
+var camelCase;
+var loaders= document.getElementById("loader");
+var content= document.getElementById("whenDone");
+function workItt(){
+    camelCase = setTimeout(showPage, 3000);
+  console.log("this works");
+
+}
+function showPage(){
+  loaders.style.display= "none";
+  whenDone.style.display= "block";
+  console.log("It worked! Yay");
+}
+workItt();
+showPage();
+</script>
 
       <script type="text/javascript">
 
@@ -29,6 +49,7 @@
             });
       });
 
-      </script>
+    </script>
+
 </body>
 </html>
